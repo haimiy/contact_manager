bd7975c42fff7d0d6d90af39c40a72cbda61f7bb
Gisacc54 <giftisacc54@gmail.com>
2020-05-10 13:29:43 +0300

    fix datainsection.php

--- db/data_insertion.php
+++ db/data_insertion.php
@@ -1,19 +1,21 @@
-<?php 
+<?php
 
 include "connection.php";
 
-$sql ="INSERT INTO genders(`gender_id`,`gender_name`) VALUES
+$sql_gender ="INSERT INTO genders(`gender_name`) VALUES
 ('Male'),
 ('Female')";
 
-$sql ="INSERT INTO roles(`role_id`,`role_name`) VALUES
+$sql_role ="INSERT INTO roles(`role_name`) VALUES
 ('Admin'),
 ('User')";
 
-$sql = "INSERT INTO users(`user_id`,`f_name`,`l_name`,`email`,`password`,`
-role_id`,`is_verified`,`is_active`,`email_verified_at`,`created_at`) VALUES
-('Khairat','Issa','Khairat096@gmail.com',SHA('1234'),1,NULL(),)";
+$sql_user = "INSERT INTO users(`f_name`,`l_name`,`email`,`password`,`role_id`,`email_verified_at`,`created_at`) VALUES
+('Khairat','Issa','Khairat096@gmail.com',SHA('1234'),1,now(),now())";
 
-$result = mysqli_query($conn,$sql);
+mysqli_query($conn,$sql_gender);
+mysqli_query($conn,$sql_role);
+$data = mysqli_query($conn,$sql_user);
+echo mysqli_error($conn);
 
  ?>
