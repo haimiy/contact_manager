ca14b4bf5b623bf1ac0b0a5c156925a8b1228772
haimiy <khairat096@gmail.com>
2020-05-10 11:17:32 +0300

    nimechange contact_manager db and insertion file

--- db/contact_manager.sql
+++ db/contact_manager.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2020 at 01:11 AM
+-- Generation Time: May 10, 2020 at 10:15 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.4
 
@@ -27,8 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cities` (
-  `city_id` int(4) NOT NULL,
-  `city_name` varchar(15) NOT NULL
+  `id` int(4) NOT NULL,
+  `name` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -38,7 +38,7 @@ CREATE TABLE `cities` (
 --
 
 CREATE TABLE `contacts` (
-  `contact_id` int(4) NOT NULL,
+  `id` int(4) NOT NULL,
   `f_name` varchar(25) NOT NULL,
   `l_name` varchar(25) NOT NULL,
   `user_id` int(4) NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE `email` (
 --
 
 CREATE TABLE `genders` (
-  `gender_id` int(4) NOT NULL,
+  `id` int(4) NOT NULL,
   `gender_name` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -99,7 +99,7 @@ CREATE TABLE `phone_numbers` (
 --
 
 CREATE TABLE `roles` (
-  `role_id` int(4) NOT NULL,
+  `id` int(4) NOT NULL,
   `role_name` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -110,16 +110,16 @@ CREATE TABLE `roles` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(4) NOT NULL,
+  `id` int(4) NOT NULL,
   `f_name` varchar(25) NOT NULL,
   `l_name` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `role_id` int(4) NOT NULL,
-  `is_verified` tinyint(1) NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  `email_verified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `email_verified_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -129,7 +129,7 @@ CREATE TABLE `users` (
 --
 
 CREATE TABLE `user_details` (
-  `user_detail_id` int(4) NOT NULL,
+  `id` int(4) NOT NULL,
   `user_id` int(4) NOT NULL,
   `gender_id` int(4) NOT NULL,
   `dob` date NOT NULL,
@@ -168,13 +168,13 @@ CREATE TABLE `user_profile` (
 -- Indexes for table `cities`
 --
 ALTER TABLE `cities`
-  ADD PRIMARY KEY (`city_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `contacts`
 --
 ALTER TABLE `contacts`
-  ADD PRIMARY KEY (`contact_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `contact_image`
@@ -192,7 +192,7 @@ ALTER TABLE `email`
 -- Indexes for table `genders`
 --
 ALTER TABLE `genders`
-  ADD PRIMARY KEY (`gender_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `phone_numbers`
@@ -204,19 +204,19 @@ ALTER TABLE `phone_numbers`
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
-  ADD PRIMARY KEY (`role_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_details`
 --
 ALTER TABLE `user_details`
-  ADD PRIMARY KEY (`user_detail_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_phone_numbers`
@@ -230,6 +230,65 @@ ALTER TABLE `user_phone_numbers`
 ALTER TABLE `user_profile`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `contact_image`
+--
+ALTER TABLE `contact_image`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `email`
+--
+ALTER TABLE `email`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `genders`
+--
+ALTER TABLE `genders`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `phone_numbers`
+--
+ALTER TABLE `phone_numbers`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_phone_numbers`
+--
+ALTER TABLE `user_phone_numbers`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- db/data_insertion.php
+++ db/data_insertion.php
@@ -2,16 +2,18 @@
 
 include "connection.php";
 
-$sql =mysql_query($conn,"INSERT INTO genders(`gender_id`,`gender_name`) VALUES
-(001,'Male'),
-(002,'Female')");
+$sql ="INSERT INTO genders(`gender_id`,`gender_name`) VALUES
+('Male'),
+('Female')";
 
-$sql =mysql_query($conn,"INSERT INTO roles(`role_id`,`role_name`) VALUES
-(001,'Admin'),
-(002,'User')");
+$sql ="INSERT INTO roles(`role_id`,`role_name`) VALUES
+('Admin'),
+('User')";
 
-$sql =mysql_query($conn,"INSERT INTO users(`user_id`,`f_name`,`l_name`,`email`,`password`,`
+$sql = "INSERT INTO users(`user_id`,`f_name`,`l_name`,`email`,`password`,`
 role_id`,`is_verified`,`is_active`,`email_verified_at`,`created_at`) VALUES
-(001,'Khairat','Issa','Khairat096@gmail.com','1234',001,'true','')");
+('Khairat','Issa','Khairat096@gmail.com',SHA('1234'),1,NULL(),)";
+
+$result = mysqli_query($conn,$sql);
 
  ?>
