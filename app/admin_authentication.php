33dc1775d695262ee536509f73ed9217889a9d57
haimiy <khairat096@gmail.com>
2020-05-13 09:45:37 +0300

    add admin_page with dataTable

--- app/admin_authentication.php
+++ app/admin_authentication.php
@@ -1,5 +1,4 @@
 <?php
-	session_start();
 	require "../db/connection.php";
 
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -7,14 +6,13 @@
 		$email = $_POST['email'];
 	 	$password = $_POST['password']; 
 
-	    $sql = "SELECT email,password from users where email = 'Khairat096@gmail.com' and password = SHA('1234')";
+	    $sql = "SELECT email,password from users where email = '$email' and password = SHA('$password')";
 		$result = mysqli_query($conn,$sql);
+
 		  	if (mysqli_num_rows($result)>0) {
-		  		echo "Success";
 		  		header("location: ../public/admin_page.php");
 		  	}
 		  	else{
-		  		echo "Fail";
 		  		header("location: ../public/admin_login.php");
 		  	}		
 	
--- public/admin_page.php
+++ public/admin_page.php
@@ -1,9 +1,92 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Admin Page</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="../resources/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../resources/css/styles.css">
 </head>
 <body>
-<h1>WELCOME</h1>
+	<div class="d-flex" id="wrapper">
+
+    <!-- Start Sidebar -->
+    <div class=" border-right" id="sidebar-wrapper" style="background-color: #bebebc;">
+	    <div class="sidebar-heading"><h2 style="text-align: center;">Admin</h2></div>
+	    <div class="list-group list-group-flush">
+	        <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+	        <a href="#" class="list-group-item list-group-item-action" style="background-color: #bebebc">Profile</a>
+	    </div>
+  	</div>
+  	<!-- Finish Sidebar -->
+	
+	<!-- Start page-content-->
+	<div id="page-content-wrapper">
+	<nav class="navbar navbar-expand-lg navbar-light bg-secondary border-bottom">
+        <div class="navbar-nav ml-auto mt-2 mt-lg-0">
+           <form class="form-inline my-2 my-lg-0">
+      		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      		<button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    		</form>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+    <nav aria-label="breadcrumb">
+  		<ul class="breadcrumb list-unstyled" style="background-color: #fff;">
+    		<li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+    		<li class="breadcrumb-item active" aria-current="page">User view</li>
+  		</ul>
+	</nav>
+    </div>
+    <hr><br><br>
+
+<div class="add-button">
+	<button type="button" class="btn btn-secondary"><a href="#">Add new User</a></button>
+</div>
+<br>
+<div class="table-responsive">  
+    <table class="table table-striped table-bordered">  
+	    <thead>  
+	        <tr>  
+	            <th>id</th>  
+	            <th>First name</th>  
+	            <th>Last name</th>  
+	            <th>Email</th>
+	            <th>Action</th>    
+	        </tr>  
+	    </thead>  
+        <?php  
+        	include "../db/connection.php";
+
+        	$sql = "SELECT id,f_name,l_name,email from users where f_name ='Khairat'and l_name='Issa'and email='Khairat096@gmail.com'";
+        	$result = mysqli_query($conn,$sql);
+
+            while($row = mysqli_fetch_array($result))  
+            {  
+                echo '  
+                    <tr>  
+                    <td>'.$row["id"].'</td>  
+                    <td>'.$row["f_name"].'</td>  
+                    <td>'.$row["l_name"].'</td>  
+                    <td>'.$row["email"].'</td>    
+                    </tr>  
+                    ';  
+            }  
+        ?>  
+    </table>  
+    </div>  
+    </div>
+    <!-- page-content-->
+    </div>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+	<script src="../resources/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../resources/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="../resources/jquery/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  	
 </html>
